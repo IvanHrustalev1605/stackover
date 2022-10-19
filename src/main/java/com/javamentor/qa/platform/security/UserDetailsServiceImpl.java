cf05f05485d9d4bede33cd6db431be08a7c232db
Dmitry Tokarev <tokareff.dm@gmail.com>
2022-10-19 15:11:21 +0000

    Resolve "Настроить Security"

--- src/main/java/com/javamentor/qa/platform/security/UserDetailsServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/security/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.javamentor.qa.platform.security;
 
 import com.javamentor.qa.platform.dao.abstracts.model.UserDao;
 import com.javamentor.qa.platform.models.entity.user.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,13 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserDao userDao;
-
-    public UserDetailsServiceImpl(UserDao userDao) {
-        this.userDao = userDao;
-    }
 
     @Override
     @Transactional
