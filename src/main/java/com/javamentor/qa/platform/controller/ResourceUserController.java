1ad65dd71626979a7246925036ced2fbe711a11b
Omar Osmanov <ooogarant1@mail.ru>
2022-10-21 14:32:12 +0300

    Bugs solved

--- src/main/java/com/javamentor/qa/platform/controller/ResourceUserController.java
+++ src/main/java/com/javamentor/qa/platform/controller/ResourceUserController.java
@@ -1,28 +1,39 @@
 package com.javamentor.qa.platform.controller;
 
-import com.javamentor.qa.platform.dao.impl.dto.UserDtoDaoImpl;
 import com.javamentor.qa.platform.models.dto.UserDto;
+import com.javamentor.qa.platform.service.abstracts.repository.ReadOnlyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
-@RequestMapping
 @RestController
+@RequestMapping("/api/user")
 public class ResourceUserController {
 
-    private final UserDtoDaoImpl userDtoDaoImpl;
+    private final ReadOnlyService<UserDto, Long> readOnlyService;
 
-    public ResourceUserController(UserDtoDaoImpl userDtoDaoImpl) {
-        this.userDtoDaoImpl = userDtoDaoImpl;
+    public ResourceUserController(ReadOnlyService<UserDto, Long> readOnlyService) {
+        this.readOnlyService = readOnlyService;
     }
 
-    @RequestMapping("/api/user/{id}")
-    ResponseEntity<Optional<UserDto>> getUserDtoById (@PathVariable long id) {
-        return new ResponseEntity<>(userDtoDaoImpl.getUserDtoById(id), HttpStatus.OK);
+    @GetMapping("/{id}")
+    @Operation(summary = "Get User Dto By Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get User обработан успешно"),
+            @ApiResponse(responseCode = "404", description = "User By Id не найден")
+    })
+    ResponseEntity<UserDto> getUserDtoById(@PathVariable long id) {
+        if (readOnlyService.getById(id).isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(readOnlyService.getById(id).get(), HttpStatus.OK);
     }
 
 }
+
--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
@@ -2,6 +2,7 @@ package com.javamentor.qa.platform.dao.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
 import com.javamentor.qa.platform.dao.impl.repository.ReadWriteDaoImpl;
+import com.javamentor.qa.platform.dao.util.SingleResultUtil;
 import com.javamentor.qa.platform.models.dto.UserDto;
 import org.springframework.stereotype.Repository;
 
@@ -15,12 +16,9 @@ public class UserDtoDaoImpl extends ReadWriteDaoImpl<UserDto, Long> implements U
     @PersistenceContext
     private EntityManager entityManager;
 
-    public Optional<UserDto> getUserDtoById(long id) {
-        try {
-            return Optional.of(entityManager.createQuery("SELECT u FROM User u WHERE u.id=:id", UserDto.class)
-                    .setParameter("id", id).getSingleResult());
-        } catch (RuntimeException e) {
-            throw new RuntimeException("User not found");
-        }
+    @Override
+    public Optional<UserDto> getById(Long id) {
+        return SingleResultUtil.getSingleResultOrNull(entityManager.createQuery("SELECT u FROM User u WHERE u.id=:id", UserDto.class)
+                .setParameter("id", id));
     }
 }
