697eb9dc03b96c0dd21d15b97785aab1711f6ea0
Tim <timur555@yandex.ru>
2022-10-26 12:21:11 +0300

    refactor

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/AnswerService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/AnswerService.java
@@ -2,9 +2,6 @@ package com.javamentor.qa.platform.service.abstracts.model;
 
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.repository.ReadWriteService;
-import org.springframework.http.ResponseEntity;
 
 public interface AnswerService extends ReadWriteService<Answer, Long> {
-
-    ResponseEntity<Object> checkAndDeleteAnswerById(Long id);
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/model/AnswerServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/AnswerServiceImpl.java
@@ -4,7 +4,6 @@ import com.javamentor.qa.platform.dao.abstracts.repository.ReadWriteDao;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.impl.repository.ReadWriteServiceImpl;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,15 +11,6 @@ public class AnswerServiceImpl extends ReadWriteServiceImpl<Answer, Long> implem
 
     public AnswerServiceImpl(ReadWriteDao<Answer, Long> readWriteDao) {
         super(readWriteDao);
-    }
-
-    @Override
-    public ResponseEntity<Object> checkAndDeleteAnswerById(Long id) {
-        if(super.existsById(id)) {
-            super.deleteById(id);
-            return ResponseEntity.ok("Entity deleted");
-        }
-        return ResponseEntity.notFound().build();
     }
 
 }
