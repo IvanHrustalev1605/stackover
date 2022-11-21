646797de1617170dd04d9bfb6429bb8f17f7e84a
Sergey Kazmin <kazmin5796@mail.ru>
2022-11-21 07:08:31 +0000

    Resolve "Написать интеграционный тест для метода voteDownForAnswer в ResourceAnswerController"

--- src/test/java/com/javamentor/qa/platform/controller/ResourceAnswerControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/controller/ResourceAnswerControllerTest.java
@@ -3,9 +3,13 @@ package com.javamentor.qa.platform.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.models.entity.user.User;
+import com.javamentor.qa.platform.models.entity.user.reputation.Reputation;
 import com.javamentor.qa.platform.service.abstracts.dto.AnswerDtoService;
+import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
+import com.javamentor.qa.platform.service.abstracts.model.ReputationService;
 import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import com.javamentor.qa.platform.util.BaseTest;
 import org.assertj.core.api.Assertions;
@@ -18,14 +22,19 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class ResourceAnswerControllerTest extends BaseTest {
+class ResourceAnswerControllerTest extends BaseTest{
 
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
     @Autowired
@@ -34,6 +43,11 @@ class ResourceAnswerControllerTest extends BaseTest {
     private AnswerDtoService answerDtoService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private AnswerService answerService;
+
+    @Autowired
+    private ReputationService reputationService;
 
 
     @Test
@@ -53,4 +67,25 @@ class ResourceAnswerControllerTest extends BaseTest {
                 .getAllAnswersDtoByQuestionId(ArgumentMatchers.eq(1L), ArgumentMatchers.eq(user.getId()));
     }
 
+    @Test
+    @DataSet(value = "datasets/answer/resource_answer_controller.yml")
+    void voteDownForAnswerTest() throws Exception {
+        User user = userService.getByEmail("email@mail.com").get();
+        Answer answer = answerService.getById(100L).get();
+        Question question = questionService.getById(100L).get();
+        Reputation reputation = reputationService.getById(100L).get();
+        Assertions.assertThat(answer.getVoteAnswers().stream().count()).isEqualTo(0);
+        Assertions.assertThat(reputationService.getById(100L).get().getCount()).isEqualTo(0);
+        this.mockMvc.perform(post("/api/user/question/{questionId}/answer/{id}/downVote", question.getId(), answer.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", getToken(user)))
+                .andDo(print())
+                .andExpect(status().isOk());
+        Assertions.assertThat(answerService.getById(100L).get()).isEqualTo(answer);
+        Assertions.assertThat(answer.getVoteAnswers().stream().count()).isEqualTo(1);
+        Assertions.assertThat(answer.getVoteAnswers().get(0).getVoteType()).isEqualTo(1);
+        Assertions.assertThat(reputationService.getById(100L).get().getCount()).isEqualTo(reputation.getCount() - 5);
+        Assertions.assertThat(user).isNotEqualTo(answer.getUser());
+        Assertions.assertThat(answerService.getById(100L)).isPresent();
+    }
 }
