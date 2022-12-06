ed85a851b4fcb208e9c03548dc512c3d687998d6
iTim555 <timur555@yandex.ru>
2022-12-06 10:24:26 +0300

    ref

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -1,6 +1,8 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +21,9 @@ public class ResourceAnswerController {
     }
 
     @DeleteMapping(value = "/{answerId}")
+    @Operation(summary = "Помечает ответ на удаление")
+    @ApiResponse(responseCode = "200", description = "Вопрос успешно помечен на удаление")
+    @ApiResponse(responseCode = "403", description = "Вопрос не найден")
     public ResponseEntity<?> markAnswerToDelete(@PathVariable("answerId") Long answerId) {
         if (answerService.getById(answerId).isPresent()) {
             answerService.markAnswerAsDelete(answerId);
