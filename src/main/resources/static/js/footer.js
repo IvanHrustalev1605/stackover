67baca867cd4dff624713554f59bfecdb63323a7
Алексей Вахрин <aavakhrin@gmail.com>
2023-01-31 05:47:31 +0000

    Resolve "Написать на js header, footer, sidebar"

---
+++ src/main/resources/static/js/footer.js
@@ -0,0 +1,9 @@
+const footerHTML = `
+    <footer id="footer" class="site-footer js-footer" role="contentinfo">
+        <div class="site-footer--container">
+            
+        </div>
+    </footer>
+`;
+
+document.body.insertAdjacentHTML("beforeend", footerHTML);
