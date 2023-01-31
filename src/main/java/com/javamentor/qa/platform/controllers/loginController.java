97b685ecf9b2f8b2d6a5b1833d519fffc3b5a613
exkot00 <114568769+exkot00@users.noreply.github.com>
2023-01-31 22:49:32 +0500

    .login

---
+++ src/main/java/com/javamentor/qa/platform/controllers/loginController.java
@@ -0,0 +1,13 @@
+package com.javamentor.qa.platform.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class loginController {
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+}
+
