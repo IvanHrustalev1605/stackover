098f7fabc4586e364c0ee749db8c2aca1cd6270d
yaroslav <ylatushkov@yahoo.com>
2023-02-04 15:31:44 +0300

    add document comment

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceQuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceQuestionController.java
@@ -4,6 +4,7 @@ import com.javamentor.qa.platform.dao.abstracts.dto.QuestionDtoDao;
 import com.javamentor.qa.platform.dao.impl.dto.QuestionDtoDaoImpl;
 import com.javamentor.qa.platform.models.dto.QuestionDto;
 import com.javamentor.qa.platform.models.entity.user.User;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,7 @@ public class ResourceQuestionController {
         this.questionDtoDao = questionDtoDao;
     }
 
+    @ApiOperation("api возвращает QuestionDto по id вопроса")
     @GetMapping("/{id}")
     public ResponseEntity<Optional<QuestionDto>> getQuestionDto(@PathVariable("id") long id,
                                                                 @AuthenticationPrincipal User user) {
