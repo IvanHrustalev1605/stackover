c3976e51588077aad31fc988df025775d6e4b93c
iTim555 <timur555@yandex.ru>
2023-02-08 15:36:36 +0300

    ref

--- src/main/java/com/javamentor/qa/platform/webapp/configs/JmApplication.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/JmApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan("com.javamentor.qa.platform")
 @EntityScan("com.javamentor.qa.platform.models.entity")
 public class JmApplication {
