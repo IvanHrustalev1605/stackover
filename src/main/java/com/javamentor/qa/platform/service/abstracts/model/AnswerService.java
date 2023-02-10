6c8db9e7df43ae1c7d52e73cd71e50663dc72636
George Pliev <tmwf_13@vk.com>
2023-02-10 09:04:19 +0000

    Resolve "Разработать api который помечает ответ на удаление #19"

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/AnswerService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/AnswerService.java
@@ -2,10 +2,13 @@ package com.javamentor.qa.platform.service.abstracts.model;
 
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.repository.ReadWriteService;
+import javassist.NotFoundException;
 
 import java.util.Optional;
 
 public interface AnswerService extends ReadWriteService<Answer, Long> {
 
     Optional<Answer> getAnswerByIdAndUserId(Long answerId, Long userId);
+
+    void markAnswerAsDeletedById(Long id) throws NotFoundException;
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/model/AnswerServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/AnswerServiceImpl.java
@@ -5,6 +5,7 @@ import com.javamentor.qa.platform.dao.abstracts.repository.ReadWriteDao;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.impl.repository.ReadWriteServiceImpl;
+import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -22,5 +23,19 @@ public class AnswerServiceImpl extends ReadWriteServiceImpl<Answer, Long> implem
     @Override
     public Optional<Answer> getAnswerByIdAndUserId(Long answerId, Long userId) {
         return answerDao.getAnswerByIdAndUserId(answerId, userId);
+    }
+
+    @Override
+    public void markAnswerAsDeletedById(Long id) throws NotFoundException {
+       if (answerDao.getById(id).isEmpty()) {
+           throw new NotFoundException(String.format("The answer with id = %s does not exist", id));
+       } else {
+           answerDao.getById(id).ifPresent(x -> {
+               if (!x.getIsDeleted()) {
+                   x.setIsDeleted(true);
+                   answerDao.update(x);
+               }
+           });
+       }
     }
 }
