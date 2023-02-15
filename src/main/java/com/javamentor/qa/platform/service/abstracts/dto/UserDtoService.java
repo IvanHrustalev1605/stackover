b1d8c2a32eea64c700fa28ce60b50f3e90a60a82
yaroslav <ylatushkov@yahoo.com>
2023-02-15 21:25:10 +0300

    refactoring

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/UserDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/UserDtoService.java
@@ -1,12 +1,11 @@
 package com.javamentor.qa.platform.service.abstracts.dto;
 
 import com.javamentor.qa.platform.models.dto.UserDto;
-import com.javamentor.qa.platform.service.abstracts.repository.ReadWriteService;
 
 import java.util.Optional;
 
-public interface UserDtoService extends ReadWriteService<UserDto, Long> {
-
+public interface UserDtoService {
 
     Optional<UserDto> getUserDtoById(Long id);
+
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
@@ -1,27 +1,22 @@
 package com.javamentor.qa.platform.service.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
-import com.javamentor.qa.platform.dao.abstracts.repository.ReadWriteDao;
 import com.javamentor.qa.platform.models.dto.UserDto;
 import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
-import com.javamentor.qa.platform.service.impl.repository.ReadWriteServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserDtoServiceImpl extends ReadWriteServiceImpl<UserDto, Long> implements UserDtoService {
+@RequiredArgsConstructor
+public class UserDtoServiceImpl implements UserDtoService {
 
     private final UserDtoDao userDtoDao;
-
-    public UserDtoServiceImpl(ReadWriteDao<UserDto, Long> readWriteDao, UserDtoDao userDtoDao) {
-        super(readWriteDao);
-        this.userDtoDao = userDtoDao;
-    }
-
 
     @Override
     public Optional<UserDto> getUserDtoById(Long id) {
         return userDtoDao.getById(id);
     }
+
 }
