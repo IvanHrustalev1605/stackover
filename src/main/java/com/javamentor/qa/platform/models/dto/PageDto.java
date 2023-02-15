f5e0ec4cff412ca577312baa3691e5ad3c0ffe86
yaroslav <ylatushkov@icloud.com>
2023-02-15 14:55:47 +0300

    init struct

--- src/main/java/com/javamentor/qa/platform/models/dto/PageDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/PageDto.java
@@ -1,20 +1,14 @@
 package com.javamentor.qa.platform.models.dto;
 
-
-import com.javamentor.qa.platform.models.dto.question.answer.AnswerDto;
-import com.javamentor.qa.platform.models.entity.user.User;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class PageDto<T> {
     private int totalResultCount;
     private int itemsOnPage;
@@ -22,29 +16,6 @@ public class PageDto<T> {
     private int totalPageCount;
     private List<T> items;
 
-    public PageDto() {
-        this.items = new ArrayList<>();
-    }
-
-    public PageDto(List<T> items, int currentPageNumber, int itemsOnPage, int totalResultCount) {
-        this.items = items.subList(currentPageNumber * itemsOnPage, currentPageNumber * itemsOnPage + itemsOnPage);
-        this.currentPageNumber = currentPageNumber;
-        this.itemsOnPage = itemsOnPage;
-        this.totalResultCount = totalResultCount;
-        this.totalPageCount = this.totalResultCount / this.itemsOnPage;
-    }
-
-//    Example use
-//    @GetMapping("/{currentPageNumber}/{itemsOnPage}")
-//    public ResponseEntity<PageDto<AnswerDto>> getPageAnswer(@PathVariable("questionId") Long questionId,
-//                                                        @AuthenticationPrincipal User user,
-//                                                        @PathVariable int currentPageNumber,
-//                                                        @PathVariable int itemsOnPage) {
-//        PageDto<AnswerDto> pageAnswerDto = new PageDto<>(
-//                answerDtoService.getAllAnswersDtoByQuestionId(questionId, 1L).orElseThrow(IllegalArgumentException::new),
-//                currentPageNumber, itemsOnPage, 6);
-//        return new ResponseEntity<>(pageAnswerDto, HttpStatus.OK);
-//    }
 }
 
 
