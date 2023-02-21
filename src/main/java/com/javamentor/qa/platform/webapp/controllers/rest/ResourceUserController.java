6510decaf4f805fa9d4be87c5b128157184c8b37
yaroslav <ylatushkov@icloud.com>
2023-02-21 12:08:50 +0300

    some refactoring

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceUserController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceUserController.java
@@ -7,12 +7,15 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -27,7 +30,9 @@ public class ResourceUserController {
             @ApiResponse(code = 404, message = "NOT FOUND - Пользователь с таким id не найден")
     })
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserDto> getUserDtoById(@ApiParam("id пользователя") @PathVariable("id") Long id) {
-        return ResponseEntity.ok(userDtoService.getUserDtoById(id));
+    public ResponseEntity<Optional<UserDto>> getUserDtoById(@ApiParam("id пользователя") @PathVariable("id") Long id) {
+        return Optional.ofNullable(userDtoService.getUserDtoById(id))
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
