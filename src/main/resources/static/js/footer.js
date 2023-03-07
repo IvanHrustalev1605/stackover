a05271cc65ecbb7d07e9742e313275dc12cc8c25
Евгений Васильев <eugene.vasilev2015@gmail.com>
2023-03-07 14:47:04 +0000

    Resolve "Написать на js header, footer, sidebar"

---
+++ src/main/resources/static/js/footer.js
@@ -0,0 +1,81 @@
+document.body.innerHTML += `
+    <footer class="text-center text-lg-start bg-light text-muted pt-2">
+        <!-- Section: Links  -->
+        <section class="footer-section">
+            <div class="container text-center text-md-start mt-5"  >
+                <!-- Grid row -->
+                <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            <i class="fas fa-gem me-3"></i>Kata
+                        </h6>
+                        <p>
+                            Eдинственная в России школа программирования, которая инвестирует в своих студентов.
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Stackover
+                        </h6>
+                        <p>
+                            <a href="\questions" class="text-reset">Вопросы</a>
+                        </p>
+                        <p>
+                            <a href="\tags" class="text-reset">Метки</a>
+                        </p>
+                        <p>
+                            <a href="\/users" class="text-reset">Пользователи</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Академия Ката
+                        </h6>
+                        <p>
+                            <a href="https://kata.academy/" class="text-reset">Сайт академии</a>
+                        </p>
+                        <p>
+                            <a href="https://kata.academy/community" class="text-reset">Community</a>
+                        </p>
+                        <p>
+                            <a href="https://kata.academy/faq" class="text-reset">FAQ</a>
+                        </p>
+
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">Контакты</h6>
+                        <p><i class="fas fa-home me-3"></i>СПб, Выборгская набережная, 29</p>
+                        <p>
+                            <i class="fas fa-envelope me-3"></i>
+                            info@kata.academy
+                        </p>
+                        <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2023 Copyright:
+            <a class="text-reset fw-bold" href="https://kata.academy/">kata.academy</a>
+        </div>
+        <!-- Copyright -->
+    </footer>`
