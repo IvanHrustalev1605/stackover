d79f47f8809cbd1aa4652194b3d02fe034c12060
Tpojlb1 <118221543+Tpojlb1@users.noreply.github.com>
2023-03-10 14:59:34 +0700

    Resolve "Реализовать api проголосовать за Answer, увеличивает оценку ответа #20

--- src/main/java/com/javamentor/qa/platform/controller/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/controller/ResourceAnswerController.java
@@ -5,7 +5,8 @@ import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.impl.model.VoteAnswerServiceImpl;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,20 +21,23 @@ import java.util.Optional;
 @RequestMapping("api/user/question/{questionId}/answer/{id}")
 @Api(description = "Контроллер для ")
 public class ResourceAnswerController {
-private final VoteAnswerServiceImpl voteAnswerService;
-private final AnswerService answerService;
-public ResourceAnswerController(VoteAnswerServiceImpl voteAnswerService, AnswerService answerService) {
-    this.voteAnswerService = voteAnswerService;
-    this.answerService = answerService;
-}
+    private final VoteAnswerServiceImpl voteAnswerService;
+    private final AnswerService answerService;
+    public ResourceAnswerController(VoteAnswerServiceImpl voteAnswerService, AnswerService answerService) {
+        this.voteAnswerService = voteAnswerService;
+        this.answerService = answerService;
+    }
 
-@PostMapping("/upVote")
-@ApiOperation("Увеличение оценки ответа")
-public ResponseEntity<Long> upVote(@PathVariable("id") Long answerId, @AuthenticationPrincipal User user){
-    Optional<Answer> answerOptional = answerService.getAnswerForVote(answerId, user.getId());
-    return answerOptional.map(
-                    answer -> new ResponseEntity<>(voteAnswerService.upVote(answer, user), HttpStatus.OK))
-            .orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
-}
+    @PostMapping("/upVote")
+    @ApiResponse(responseCode = "400", description = "Не удалось проголосовать")
+    public ResponseEntity<Long> upVote(@Parameter(description = "Уникальный id ответа")
+                                       @PathVariable("id") Long answerId,
+                                       @Parameter(description = "Пользователь прошедший аутентификацию")
+                                       @AuthenticationPrincipal User user){
+        Optional<Answer> answerOptional = answerService.getAnswerForVote(answerId, user.getId());
+        return answerOptional.map(
+                        answer -> new ResponseEntity<>(voteAnswerService.upVote(answer, user), HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
+    }
 }
 
