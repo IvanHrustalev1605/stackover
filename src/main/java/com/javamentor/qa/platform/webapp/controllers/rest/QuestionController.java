6b500d7c0e05084efcb3fa0ca4344667a83f36d8
KupchenkoArtur <kupchenko.artur@gmail.com>
2023-03-14 20:28:46 +0300

    Разработать api которое считает количество вопросов в базе #40

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionController.java
@@ -1,6 +1,9 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +22,11 @@ public class QuestionController {
     }
 
     @GetMapping("/count")
+    @ApiOperation(value = "Получаем количество вопросов в базе")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Количество вопросов успешно получено"),
+            @ApiResponse(code = 400, message = "Неправильный запрос")
+    })
     public ResponseEntity<Long> getCountQuestion() {
         Optional<Long> count = questionService.getCountQuestion();
         return count.map(aLong -> new ResponseEntity<>(aLong, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
