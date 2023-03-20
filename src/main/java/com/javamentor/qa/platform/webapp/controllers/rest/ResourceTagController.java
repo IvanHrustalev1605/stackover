8a270ea03ad0365cc030d810149df20dca703fb4
KupchenkoArtur <kupchenko.artur@gmail.com>
2023-03-20 10:02:02 +0300

    create RelatedTagDto.java and TagDtoDaoImpl.java

---
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceTagController.java
@@ -0,0 +1,36 @@
+package com.javamentor.qa.platform.webapp.controllers.rest;
+
+import com.javamentor.qa.platform.models.dto.RelatedTagDto;
+import com.javamentor.qa.platform.service.abstracts.dto.TagDtoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/user/tag/related")
+public class ResourceTagController {
+    private final TagDtoService tagDtoService;
+
+    public ResourceTagController(TagDtoService tagDtoService) {
+        this.tagDtoService = tagDtoService;
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Получает список топ 10 DTO тегов")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Список тегов DTO успешно получен"),
+            @ApiResponse(code = 400, message = "Неправильный запрос")
+    })
+    public ResponseEntity<List<RelatedTagDto>> getAllAnswers() {
+        Optional<List<RelatedTagDto>> relatedTagDtoList = tagDtoService.getTopTags();
+        return relatedTagDtoList.map(relatedTagDos -> new ResponseEntity<>(relatedTagDos, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
