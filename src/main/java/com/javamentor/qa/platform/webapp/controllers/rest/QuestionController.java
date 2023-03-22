296b6a84d3251d914c462736fddae3e1ae6319f4
iTim555 <timur555@yandex.ru>
2023-03-22 16:30:02 +0300

    ref

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionController.java
@@ -1,21 +1,15 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
 import com.javamentor.qa.platform.models.dto.QuestionCommentDto;
-import com.javamentor.qa.platform.models.entity.question.Question;
-import com.javamentor.qa.platform.models.entity.question.VoteType;
-import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.dto.CommentDtoService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
-import com.javamentor.qa.platform.service.abstracts.model.VoteQuestionService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,12 +21,10 @@ import java.util.Optional;
 public class QuestionController {
 
     private final CommentDtoService commentDtoService;
-    private final VoteQuestionService voteQuestionService;
     private final QuestionService questionService;
 
-    public QuestionController(CommentDtoService commentDtoService, VoteQuestionService voteQuestionService, QuestionService questionService) {
+    public QuestionController(CommentDtoService commentDtoService, QuestionService questionService) {
         this.commentDtoService = commentDtoService;
-        this.voteQuestionService = voteQuestionService;
         this.questionService = questionService;
     }
 
