39e4a43c2c8a9839802b7553041063a233054f4d
KupchenkoArtur <kupchenko.artur@gmail.com>
2023-03-23 08:25:42 +0300

    Change method getJWTToken

--- src/test/java/com/javamentor/qa/platform/BaseTest.java
+++ src/test/java/com/javamentor/qa/platform/BaseTest.java
@@ -9,6 +9,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,10 +24,14 @@ public abstract class BaseTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     public String getTokenJWT(User user) {
-        return JwtService.generateToken(user);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
+        return JwtService.generateToken(userDetails);
     }
+
 
 
 }
