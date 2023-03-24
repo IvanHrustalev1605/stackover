76d322a496e52e3ab80e5466358e4f21c6c5bf29
iTim555 <timur555@yandex.ru>
2023-03-24 10:32:31 +0300

    ref

--- src/test/java/com/javamentor/qa/platform/BaseTest.java
+++ src/test/java/com/javamentor/qa/platform/BaseTest.java
@@ -4,52 +4,48 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.junit5.api.DBRider;
 import com.javamentor.qa.platform.models.dto.TokenResponseDTO;
-import com.javamentor.qa.platform.security.jwt.JwtService;
-import com.javamentor.qa.platform.service.abstracts.model.RoleService;
-import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import com.javamentor.qa.platform.webapp.configs.JmApplication;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Objects;
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@DBRider
-@DBUnit
 @AutoConfigureMockMvc
+@AutoConfigureTestEntityManager
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource(locations = "classpath:application.properties")
 @SpringBootTest(classes = JmApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@DBRider
+@DBUnit(caseSensitiveTableNames = true, allowEmptyFields = true, schema = "public")
 public abstract class BaseTest {
+
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
-    @Autowired
-    private RoleService roleService;
-    @Autowired
-    private UserService userService;
 
     public String getTokenJWT(String email, String password) throws Exception {
 
-        TokenResponseDTO tokenResponseDTO = new TokenResponseDTO(roleService.getByName("ROLE_USER").orElseThrow().getName(),
-                JwtService.generateToken(userService.getByEmail(email).orElseThrow()));
-
-        return Objects.requireNonNull(mockMvc.perform(post("/api/auth/token")
-                        .with(httpBasic(email, password))
-                        .content(objectMapper.writeValueAsString(tokenResponseDTO))
-                        .header("Authorization", "Bearer " + tokenResponseDTO.getToken()))
-                .andReturn().getResponse()
-                .getHeader("Authorization")).substring(7);
+        return "Bearer " + JsonPath.read((mockMvc.perform(post("/api/auth/token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new TokenResponseDTO(email, password))))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn()).getResponse().getContentAsString(), "token");
     }
-
-
 }
