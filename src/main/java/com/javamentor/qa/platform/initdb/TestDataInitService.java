346b21629d0cc3311b7af74901a1e95b0e048c53
gadzhief <gadzhief@gmail.com>
2023-04-19 13:19:08 +0300

    Добавлена инициализация данных

--- src/main/java/com/javamentor/qa/platform/initdb/TestDataInitService.java
+++ src/main/java/com/javamentor/qa/platform/initdb/TestDataInitService.java
@@ -1,24 +1,36 @@
 package com.javamentor.qa.platform.initdb;
 
+import com.javamentor.qa.platform.dao.impl.model.RoleDaoImpl;
+import com.javamentor.qa.platform.dao.impl.model.UserDaoImpl;
 import com.javamentor.qa.platform.models.entity.question.Question;
 import com.javamentor.qa.platform.models.entity.question.Tag;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
+import com.javamentor.qa.platform.models.entity.user.Role;
 import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.models.entity.user.reputation.Reputation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TestDataInitService {
+
+    private final UserDaoImpl userDao;
+    private final RoleDaoImpl roleDao;
 
 
     @Transactional
     public void createEntity() {
+
+        createRoles();
+        createUsers();
 
     }
 
@@ -26,11 +38,30 @@ public class TestDataInitService {
         return new BCryptPasswordEncoder();
     }
 
+    private void createRoles() {
+        List<Role> roles = List.of(new Role("ADMIN"), new Role("USER"));
+
+        roleDao.persistAll(roles);
+    }
     List<User> users = new ArrayList<>();
 
     private void createUsers() {
+        var alex = new User(null, "alex@mail.com", passwordEncoder().encode("123"),
+                "Alex Duncan", LocalDateTime.now(), true, false, "NYC",
+                null, null, null, "some", null,
+                null, null, roleDao.getByName("ADMIN").orElseThrow());
 
+        var bob = new User(null, "bob@mail.com", passwordEncoder().encode("321"),
+                "Bob Jones ", LocalDateTime.now(), true, false, "Los Angeles",
+                null, null, null, "some", null,
+                null, null, roleDao.getByName("USER").orElseThrow());
+
+        users.add(alex);
+        users.add(bob);
+
+        userDao.persistAll(users);
     }
+
 
     List<Tag> tags = new ArrayList<>();
 
