d92fe830def47fcbc8ea617d41a302fa8058cfe2
docixpor <119433532+docixpor@users.noreply.github.com>
2023-04-20 13:10:31 +0300

    bootstrap main

---
+++ src/main/resources/static/js/footer.js
@@ -0,0 +1,9 @@
+let footer = `
+      <!-- Footer -->
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+          © 2021 Copyright:
+          <a class="text-reset fw-bold" href="https://kata.academy/">KATA_ACADEMY</a>
+        </div>
+
+`;
+document.getElementById('footer').innerHTML = footer
