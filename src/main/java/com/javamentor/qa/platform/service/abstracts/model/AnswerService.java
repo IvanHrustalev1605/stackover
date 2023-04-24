c96e8af68bc7ff1a50871e1abf76c98bf76910c3
Valentina Churakova <churakova3003@gmail.com>
2023-04-24 16:06:21 +0000

    Resolve "Разработать api который помечает ответ на удаление #19"

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/AnswerService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/AnswerService.java
@@ -3,6 +3,12 @@ package com.javamentor.qa.platform.service.abstracts.model;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.repository.ReadWriteService;
 
+import java.util.Optional;
+
+
 public interface AnswerService extends ReadWriteService<Answer, Long> {
 
+    public Optional<Answer> getAnswerById(Long answerId);
+
+    void markAnswerIsDelete(Optional<Answer> answerOptional);
 }
